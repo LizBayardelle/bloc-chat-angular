1b77ef260e67260978b05ba9a335752d9ac01667
Shannon Bertucci <hello@shannon.io>
2017-02-28 12:09:30 -0700

    Updates to maybe fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,29 @@
 (function() {
-    function config($stateProvider, $locationProvider) {
-    	$locationProvider
-         	.html5Mode({
-             	enabled: true,
-             	requireBase: false
-         	});
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+           enabled: true,
+           requireBase: false
+      });
 
-        $stateProvider
-         	.state('home', {
-             	url: '/',
-             	controller: 'HomeCtrl as home',
-             	templateUrl: '/templates/home.html'
-        	});
-    }
- 
-angular
-    .module('blocChat', ['ui.router', 'firebase'])
-    .config(config, function(){
-		var config = {
-		    apiKey: "AIzaSyBT3uXK6AjicRlgAfqazh-o_xAwQvFiRwg",
-		    authDomain: "blocchat-4a6ab.firebaseapp.com",
-		    databaseURL: "https://blocchat-4a6ab.firebaseio.com",
-		    storageBucket: "blocchat-4a6ab.appspot.com",
-		    messagingSenderId: "98451881"
-		};
-		firebase.initializeApp(config);
-	});
+    $stateProvider
+      .state('home', {
+           url: '/',
+           controller: 'HomeCtrl as home',
+           templateUrl: '/templates/home.html'
+      });
+      
+    var config = {
+      apiKey: "AIzaSyBT3uXK6AjicRlgAfqazh-o_xAwQvFiRwg",
+      authDomain: "blocchat-4a6ab.firebaseapp.com",
+      databaseURL: "https://blocchat-4a6ab.firebaseio.com",
+      storageBucket: "blocchat-4a6ab.appspot.com",
+      messagingSenderId: "98451881"
+    };
+    firebase.initializeApp(config);
+  }
+
+    angular
+      .module('blocChat', ['ui.router', 'firebase'])
+      .config(config);
 })();
